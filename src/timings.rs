847ebb06c602a3563677beb486593b63fa6ab126
Alex McCartney <alexmccartney@protonmail.com>
2025-02-26 14:16:36 +0000

    added "add" op functionality in timings; "edit" op not working.

--- src/timings.rs
+++ src/timings.rs
@@ -2,7 +2,7 @@ use derive_setters::Setters;
 use ratatui::{
     buffer::Buffer,
     crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
-    layout::{Constraint, Layout, Rect},
+    layout::{Constraint, Layout, Rect, Position},
     style::{
         Color, Stylize, Style
     },
@@ -75,8 +75,14 @@ impl Timing {
             timing: (String::from("09:00"), String::from("17:00"))
         }
     }
+    fn new(start: &str, end: &str) -> Timing {
+        Timing {
+            timing: (String::from(start), String::from(end))
+        }
+    }
     fn format(self) -> String {
         let mut joined = String::from(&self.timing.0);
+        joined.push_str("-");
         joined.push_str(&self.timing.1);
         joined
     }
@@ -106,6 +112,7 @@ impl TimingCollection {
     }
 }
 
+#[derive(Clone)]
 enum TimingOp {
     Add,
     Del,
@@ -172,6 +179,9 @@ pub struct TimingsWidget {
     timing_selected: usize,
     operation_selected: TimingOp,
     timing_op_list: TimingOpList,
+    input: String,
+    character_index: usize,
+    input_area: Rect,
     list_element_entries: TimingsList,
     schedule: Vec<Weekday>
 }
@@ -225,6 +235,9 @@ impl Default for TimingsWidget {
             timing_selected: 0,
             operation_selected: TimingOp::Add,
             timing_op_list: TimingOpList::default(),
+            input: String::new(),
+            character_index: 0,
+            input_area: Rect::new(0,0,0,0),
             list_element_entries: TimingsList::from_iter([
                 (Weekday::Monday(TimingCollection::default()), "Enter the start and end timings for this day."),
                 (Weekday::Tuesday(TimingCollection::default()), "Enter the start and end timings for this day."),
@@ -242,7 +255,19 @@ impl Default for TimingsWidget {
 impl TimingsWidget {
     pub fn run (mut self, mut terminal: &mut DefaultTerminal) -> Result<Timings, Box< dyn Error>> {
         while !self.should_exit {
-            terminal.draw(|f| f.render_widget(&mut self, f.area()))?;
+            terminal.draw(|f| {
+                f.render_widget(&mut self, f.area());
+                match self.current_screen {
+                    CurrentScreen::Add | CurrentScreen::Edit => {
+                        f.set_cursor_position(Position::new(
+                                self.input_area.x + self.character_index as u16 + 1,
+                                // move one line down, from the border to the input lin
+                                self.input_area.y + 1,
+                        ))
+                    },
+                    _ => {}
+                }
+            })?;
             if let Event::Key(key) = event::read()? {
                 self.handle_key(key);
                 //self.text_area.input(key);
@@ -268,7 +293,7 @@ impl TimingsWidget {
                         // add code to select the list item
                         // render popup now using current selection
                         // check the current selection
-                        let info = if let Some(i) = self.list_element_entries.state.selected() {
+                        if let Some(i) = self.list_element_entries.state.selected() {
                             self.weekday_selected = i;
                         };
 
@@ -290,7 +315,7 @@ impl TimingsWidget {
                     KeyCode::Char('l') | KeyCode::Right | KeyCode::Enter => {
                         // add code to select the list item
                         // render popup now using current selection
-                        let _change_selected_timing = if let Some(i) = self.list_element_entries.state.selected() {
+                        if let Some(i) = self.list_element_entries.list[self.weekday_selected].timings.state.selected() {
                             self.timing_selected = i;
                         };
 
@@ -312,27 +337,74 @@ impl TimingsWidget {
                     KeyCode::Char('l') | KeyCode::Right | KeyCode::Enter => {
                         // add code to select the list item
                         // render popup now using current selection
+                        if let Some(i) = self.timing_op_list.state.selected() {
+                            
+                            let op = match i {
+                                0 => TimingOp::Add,
+                                1 => TimingOp::Del,
+                                2 => TimingOp::Edit,
+                                _ => TimingOp::Add
+
+                            };
+
+                            self.operation_selected = op.clone();
+
+                            match op {
+                                TimingOp::Add => self.current_screen = CurrentScreen::Add,
+                                TimingOp::Del => self.current_screen = CurrentScreen::Delete,
+                                TimingOp::Edit => self.current_screen = CurrentScreen::Edit,
+                            };
+                        };
+
                     }
                     _ => {}
                 }
             },
             CurrentScreen::Add => {
                 match key.code {
-                    KeyCode::Char('h') | KeyCode::Left => self.reverse_state(),
-                    KeyCode::Char('q') | KeyCode::Esc => self.should_exit = true,
-                    KeyCode::Char('l') | KeyCode::Right | KeyCode::Enter => {
-                        // add code to select the list item
-                        // render popup now using current selection
+                    KeyCode::Esc => self.reverse_state(),
+                    KeyCode::Left => self.move_cursor_left(),
+                    KeyCode::Right => self.move_cursor_right(),
+                    KeyCode::Backspace => self.delete_char(),
+                    KeyCode::Char(to_insert) => self.enter_char(to_insert),
+                    KeyCode::Enter => {
+                        // add the new timing!
+                        //let re = Regex::new(r"(^\d{2}:\d{2}-\d{2}:\d{2}$|^\d{2}:\d{2}:\d{2}-\d{2}:\d{2}:\d{2}$|^\d{2}:\d{2}-\d{2}:\d{2}:\d{2}$|^\d{2}:\d{2}:\d{2}-\d{2}:\d{2}$)").unwrap();
+                        // parse the timing
+                        let new_t = self.input.as_str()
+                            .split("-")
+                            .map(|x| x.to_string())
+                            .collect::<Vec<String>>();
+                        let t = Timing::new(&new_t[0], &new_t[1]);
+
+                        self.list_element_entries.list[self.weekday_selected].timings.timing_collection.push(t);
+                        self.reverse_state();
+                        self.input.clear();
+                        self.character_index = 0;
                     }
                     _ => {}
                 }
             },
             CurrentScreen::Edit => {
                 match key.code {
-                    KeyCode::Char('h') | KeyCode::Left => self.reverse_state(),
-                    KeyCode::Char('l') | KeyCode::Right | KeyCode::Enter => {
-                        // add code to select the list item
-                        // render popup now using current selection
+                    KeyCode::Esc => self.reverse_state(),
+                    KeyCode::Left => self.move_cursor_left(),
+                    KeyCode::Right => self.move_cursor_right(),
+                    KeyCode::Backspace => self.delete_char(),
+                    KeyCode::Char(to_insert) => self.enter_char(to_insert),
+                    KeyCode::Enter => {
+                        // add the new timing!
+                        //let re = Regex::new(r"(^\d{2}:\d{2}-\d{2}:\d{2}$|^\d{2}:\d{2}:\d{2}-\d{2}:\d{2}:\d{2}$|^\d{2}:\d{2}-\d{2}:\d{2}:\d{2}$|^\d{2}:\d{2}:\d{2}-\d{2}:\d{2}$)").unwrap();
+                        // parse the timing
+                        let new_t = self.input.as_str()
+                            .split("-")
+                            .map(|x| x.to_string())
+                            .collect::<Vec<String>>();
+                        let t = Timing::new(&new_t[0], &new_t[1]);
+                        let _removed = std::mem::replace(&mut self.list_element_entries.list[self.weekday_selected].timings.timing_collection[self.timing_selected], t);
+                        self.reverse_state();
+                        self.input.clear();
+                        self.character_index = 0;
                     }
                     _ => {}
                 }
@@ -435,7 +507,56 @@ impl TimingsWidget {
 
 
     }
+    // TODO cursor movement is not yet implemented due to having to set the position on the 
+    // frame, and needed to pass the frame to the render function somehow
+    fn move_cursor_left(&mut self) {
+        let cursor_moved_left = self.character_index.saturating_sub(1);
+        self.character_index = self.clamp_cursor(cursor_moved_left);
+    }
 
+    fn move_cursor_right(&mut self) {
+        let cursor_moved_right = self.character_index.saturating_add(1);
+        self.character_index = self.clamp_cursor(cursor_moved_right);
+    }
+
+    fn enter_char(&mut self, new_char: char) {
+        let index = self.byte_index();
+        self.input.insert(index, new_char);
+        self.move_cursor_right();
+    }
+
+    fn byte_index(&self) -> usize {
+        self.input
+            .char_indices()
+            .map(|(i, _)| i)
+            .nth(self.character_index)
+            .unwrap_or(self.input.len())
+    }
+
+    fn delete_char(&mut self) {
+        let is_not_cursor_leftmost = self.character_index != 0;
+        if is_not_cursor_leftmost {
+
+            let current_index = self.character_index;
+            let from_left_to_current_index = current_index -1;
+            // get all characters before the selected character
+            let before_char_to_delete = self.input.chars().take(from_left_to_current_index);
+            // get all characters after selected character
+            let after_char_to_delete = self.input.chars().skip(current_index);
+
+            //put all the chars together except the selected one (which is "deleted")
+            self.input = before_char_to_delete.chain(after_char_to_delete).collect();
+            self.move_cursor_left();
+        }
+    }
+
+    fn clamp_cursor(&self, new_cursor_pos: usize) -> usize {
+        new_cursor_pos.clamp(0, self.input.chars().count())
+    }
+
+    fn reset_cursor(&mut self) {
+        self.character_index = 0;
+    }
 
     // rendering logic
     fn render_header(area: Rect, buf: &mut Buffer) {
@@ -517,15 +638,6 @@ impl TimingsWidget {
     }
 
     fn render_op_list(&mut self, area: Rect, buf: &mut Buffer) {
-        /*
-        let popup = Popup::default()
-                .content("hello")
-                .style(SELECTED_STYLE)
-                .title("Select a timing")
-                .border_style(ITEM_HEADER_STYLE)
-                .render(popup_area, buf);
-        */
-
         let block = Block::new()
             .title(Line::raw("Select an operation").centered())
             .borders(Borders::ALL)
@@ -553,6 +665,33 @@ impl TimingsWidget {
             .highlight_spacing(HighlightSpacing::Always);
         // we have to diferentiate this "render" from the render fn on self
         StatefulWidget::render(list, area, buf, &mut self.timing_op_list.state);
+
+    }
+    fn render_add(&self, area: Rect, buf: &mut Buffer) {
+       let input = Paragraph::new(self.input.as_str()) 
+           .style(SELECTED_STYLE)
+           .bg(NORMAL_ROW_BG)
+           .block(
+               Block::bordered()
+               .style(ITEM_HEADER_STYLE)
+               .title("Add timing")
+           )
+           .render(area, buf);
+    }
+
+    fn render_edit(&mut self, area: Rect, buf: &mut Buffer) {
+        // set the current input as the entry selected.
+        self.input = self.list_element_entries.list[self.weekday_selected].timings.timing_collection[self.timing_selected].clone().format(); 
+
+        let input = Paragraph::new(self.input.as_str()) 
+           .style(SELECTED_STYLE)
+           .bg(NORMAL_ROW_BG)
+           .block(
+               Block::bordered()
+               .style(ITEM_HEADER_STYLE)
+               .title("Add timing")
+           )
+           .render(area, buf);
 
     }
 
@@ -600,6 +739,8 @@ impl Widget for &mut TimingsWidget {
             width: area.width / 2,
             height: area.height / 3,
         };
+
+        Clear.render(area, buf);
 
         match self.current_screen {
             CurrentScreen::Weekdays => {
@@ -650,13 +791,93 @@ impl Widget for &mut TimingsWidget {
 
             },
             CurrentScreen::TimingOptions => {
+                let [header_area, main_area, footer_area] = Layout::vertical([
+                    Constraint::Length(2),
+                    Constraint::Fill(1),
+                    Constraint::Length(1),
+                ])
+                .areas(area);
+
+                let [list_area, item_area] = Layout::vertical([
+                    Constraint::Fill(3),
+                    Constraint::Fill(1)
+                ])
+                .areas(main_area);
+
+                let [weekdays_area, day_area] = Layout::horizontal([
+                    Constraint::Fill(1),
+                    Constraint::Fill(1)
+                ])
+                .areas(list_area);
+
+                TimingsWidget::render_header(header_area, buf);
+                TimingsWidget::render_footer(footer_area, buf);
+                self.render_weekdays_list(weekdays_area, buf);
+                self.render_day_list(day_area, buf);
+                self.render_selected_item(item_area, buf);
+
+
                 self.render_op_list(popup_area, buf);
            },
             CurrentScreen::Add => {
+                let [header_area, main_area, footer_area] = Layout::vertical([
+                    Constraint::Length(2),
+                    Constraint::Fill(1),
+                    Constraint::Length(1),
+                ])
+                .areas(area);
 
-                Clear.render(area, buf);
+                let [list_area, item_area] = Layout::vertical([
+                    Constraint::Fill(3),
+                    Constraint::Fill(1)
+                ])
+                .areas(main_area);
+
+                let [weekdays_area, day_area] = Layout::horizontal([
+                    Constraint::Fill(1),
+                    Constraint::Fill(1)
+                ])
+                .areas(list_area);
+
+                TimingsWidget::render_header(header_area, buf);
+                TimingsWidget::render_footer(footer_area, buf);
+                self.render_weekdays_list(weekdays_area, buf);
+                self.render_day_list(day_area, buf);
+                self.render_selected_item(item_area, buf);
+                // set the cursor area
+                self.input_area = popup_area;
+                self.render_add(popup_area, buf);
+
             },
-            CurrentScreen::Edit => {},
+            CurrentScreen::Edit => {
+                let [header_area, main_area, footer_area] = Layout::vertical([
+                    Constraint::Length(2),
+                    Constraint::Fill(1),
+                    Constraint::Length(1),
+                ])
+                .areas(area);
+
+                let [list_area, item_area] = Layout::vertical([
+                    Constraint::Fill(3),
+                    Constraint::Fill(1)
+                ])
+                .areas(main_area);
+
+                let [weekdays_area, day_area] = Layout::horizontal([
+                    Constraint::Fill(1),
+                    Constraint::Fill(1)
+                ])
+                .areas(list_area);
+
+                TimingsWidget::render_header(header_area, buf);
+                TimingsWidget::render_footer(footer_area, buf);
+                self.render_weekdays_list(weekdays_area, buf);
+                self.render_day_list(day_area, buf);
+                self.render_selected_item(item_area, buf);
+                // set the cursor area
+                self.input_area = popup_area;
+                self.render_edit(popup_area, buf);
+            },
             CurrentScreen::Delete => {},
             CurrentScreen::Exit => {},
         }
